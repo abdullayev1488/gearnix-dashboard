d3391310e1167a973e8b42264f88faaa362172ad
Elman Abdullayev <elmanabdullayev001@gmail.com>
2026-03-15 07:50:58 +0400

    implement proxy in dashboard

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,37 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import path from "path";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    svgr({
-      svgrOptions: {
-        icon: true,
-        // This will transform your SVG to a React component
-        exportType: "named",
-        namedExport: "ReactComponent",
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
       },
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(import.meta.dirname, "./src"),
     },
-  },
+    plugins: [
+      react(),
+      svgr({
+        svgrOptions: {
+          icon: true,
+          // This will transform your SVG to a React component
+          exportType: "named",
+          namedExport: "ReactComponent",
+        },
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(import.meta.dirname, "./src"),
+      },
+    },
+  };
 });
