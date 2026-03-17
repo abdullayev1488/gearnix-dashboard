f7ff1dbb60b67f5dee0274d38f3656814b718b9e
Elman Abdullayev <elmanabdullayev001@gmail.com>
2026-03-18 02:37:43 +0400

    proxy

--- vite.config.js
+++ vite.config.js
@@ -7,10 +7,28 @@ export default defineConfig({
   server: {
     port: 9001,
     host: true,
-    allowedHosts: ['admin-gearnix.elmandev.site']
+    allowedHosts: ['admin-gearnix.elmandev.site'],
+    proxy: {
+      '/api': {
+        target: process.env.VITE_API_URL,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  },
+  preview: {
+    port: 9001,
+    host: true,
+    allowedHosts: ['admin-gearnix.elmandev.site'],
+    proxy: {
+      '/api': {
+        target: process.env.VITE_API_URL,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
   },
   plugins: [
-
     react(),
     svgr({
       svgrOptions: {
